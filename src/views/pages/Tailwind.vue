e93bcaee51179b7bda146686e422c5d6b88bdcfb
gonzalo <gonzalojpv@gmail.com>
2022-06-14 23:58:09 -0500

    GP, FsLightbox

--- src/views/pages/Tailwind.vue
+++ src/views/pages/Tailwind.vue
@@ -9,7 +9,9 @@
         </a>
         <a href="#" class="nav-item active md:mb-0 md:mr-4">Cover page</a>
         <a href="#" class="nav-item md:mb-0 md:mr-4">Home</a>
-        <a href="#" class="nav-item md:mb-0 md:mr-4">Series</a>
+        <a href="#" 
+        @click.prevent="onOpenVideo"
+        class="nav-item md:mb-0 md:mr-4">Series</a>
         <a href="#" class="nav-item md:mb-0">Community</a>
       </section>
       <!-- /.Left -->
@@ -23,15 +25,42 @@
     <!-- /.Header nav -->
     <!-- Testimonials -->
     <!-- /.Testimonials -->
+    <FsLightbox
+      ref="fsLightboxRef"
+      :toggler="toggler"
+      :on-open="onOpenFsLightbox"
+      :on-show="onShow"
+      :on-init="onInit"
+      :sources="[
+        'https://www.youtube.com/watch?v=qZKEbfHY_t0&t=12s'
+      ]"
+    />
   </div>
 </template>
 
-<script>
+<script setup>
+import FsLightbox from 'fslightbox-vue/v3'
 import AtomLogo from "@/components/Atoms/Logo";
 
-export default {
-  components: {
-    AtomLogo
-  }
+import { ref } from 'vue'
+
+const fsLightboxRef = ref('')
+const toggler = ref(false)
+
+const onOpenVideo = () => {
+  console.log('New: toggler', toggler.value)
+  toggler.value = !toggler.value
+  console.log('New: toggler', toggler.value)
+  console.log('fsLightboxRef', fsLightboxRef.value)
+}
+
+const onOpenFsLightbox = () => {
+  console.log('onOpenFsLightbox')
+}
+const onShow = () => {
+  console.log('onShow')
+}
+const onInit = () => {
+  console.log('onInit')
 }
 </script>
